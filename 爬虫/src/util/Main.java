0b2fea0118aa6aad6ef5ca5321474e9d7db48b27
xgpxg <1584929962@qq.com>
2017-09-16 17:55:03 +0800

    哈哈哈哈

--- 爬虫/src/util/Main.java
+++ 爬虫/src/util/Main.java
@@ -1,9 +1,41 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-	public static void main(String[] args) {
-	 	  System.out.println("hello git!  "); 
+	public static void main(String[] args) throws IOException {
+		URL u = new URL("http://www.baidu.com");
+		URLConnection connection = u.openConnection();
+		HttpURLConnection htCon = (HttpURLConnection) connection;
+		int code = htCon.getResponseCode();
+		if (code == HttpURLConnection.HTTP_OK) {
+			System.out.println("返回状态 "+HttpURLConnection.HTTP_OK);
+			BufferedReader in = new BufferedReader(new InputStreamReader(htCon.getInputStream(), "utf-8"));
+			StringBuffer sb = new StringBuffer();
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				sb.append(inputLine + "\n");
+			in.close();
+			String regStr = RegString.MatchUrl;
+			Pattern p = Pattern.compile(regStr);
+			Matcher m = p.matcher(sb);
+			while(m.find()) {
+					System.out.println();
+				System.out.print(m.group());
+			}
+			//System.out.println(m);
+		} else {
+			System.out.println("无法连接到 "+u.toString());
+		}
 	}
 
 }
