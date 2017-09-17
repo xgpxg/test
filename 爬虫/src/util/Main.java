accf80648203db250db6afa2659a09a35f40af0d
xgpxg <1584929962@qq.com>
2017-09-17 10:04:21 +0800

    抓取网页内容

--- 爬虫/src/util/Main.java
+++ 爬虫/src/util/Main.java
@@ -7,35 +7,35 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		URL u = new URL("http://www.baidu.com");
-		URLConnection connection = u.openConnection();
-		HttpURLConnection htCon = (HttpURLConnection) connection;
-		int code = htCon.getResponseCode();
-		if (code == HttpURLConnection.HTTP_OK) {
-			System.out.println("返回状态 "+HttpURLConnection.HTTP_OK);
-			BufferedReader in = new BufferedReader(new InputStreamReader(htCon.getInputStream(), "utf-8"));
-			StringBuffer sb = new StringBuffer();
-			String inputLine;
-			while ((inputLine = in.readLine()) != null)
-				sb.append(inputLine + "\n");
-			in.close();
-			String regStr = RegString.MatchUrl;
-			Pattern p = Pattern.compile(regStr);
-			Matcher m = p.matcher(sb);
-			while(m.find()) {
-					System.out.println();
-				System.out.print(m.group());
-			}
-			//System.out.println(m);
-		} else {
-			System.out.println("无法连接到 "+u.toString());
-		}
+		HtmlUtils ht = new HtmlUtils();
+		String url = "http://www.baidu.com";
+		ht.getUrlStack(ht.getUrlStack(url));
+		/*
+		 * URL u = new URL("http://www.baidu.com"); URLConnection connection =
+		 * u.openConnection(); HttpURLConnection htCon = (HttpURLConnection) connection;
+		 * int code = htCon.getResponseCode(); if (code == HttpURLConnection.HTTP_OK) {
+		 * System.out.println("返回状态 "+HttpURLConnection.HTTP_OK); BufferedReader in =
+		 * new BufferedReader(new InputStreamReader(htCon.getInputStream(), "utf-8"));
+		 * StringBuffer sb = new StringBuffer(); String inputLine; while ((inputLine =
+		 * in.readLine()) != null) sb.append(inputLine + "\n"); in.close(); String
+		 * regStr = RegString.MatchUrl; Pattern p = Pattern.compile(regStr); Matcher m =
+		 * p.matcher(sb); while(m.find()) { System.out.println();
+		 * System.out.print(m.group()); } //System.out.println(m); } else {
+		 * System.out.println("无法连接到 "+u.toString()); }
+		 */
 	}
 
 }
